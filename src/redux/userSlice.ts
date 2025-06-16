048d5c22023cd656de79a56353519473377ba199
Alex Flok <alexflok2018@gmail.com>
2025-06-16 16:33:34 -0700

    create basic user slice and firebase auth; wip: connecting email password sign in sign up and proper navigation based on the state;

---
+++ src/redux/userSlice.ts
@@ -0,0 +1,30 @@
+import { createSlice, type PayloadAction } from "@reduxjs/toolkit";
+
+type UserProps = {
+    firstName: string;
+    lastName: string;
+    email: string;
+    age?: number
+}
+
+const initialState: UserProps = {
+    firstName: "",
+    lastName: "",
+    email: ""
+}
+
+const userSlice = createSlice({
+    name: "user",
+    initialState,
+    reducers: {
+        setUser: (state, action: PayloadAction<UserProps>) => {
+            return {...state, ...action}
+        },
+        resetUser: () => {
+           return initialState
+        }
+    }
+})
+
+export const {setUser, resetUser} = userSlice.actions
+export default userSlice.reducer
